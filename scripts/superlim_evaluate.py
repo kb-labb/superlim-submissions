10c15b3e1d0eb47b1f349ef24cbcde844fd62cd1
Robin Kurtz <robin.kurtz@kb.se>
2023-03-21 13:59:05 +0100

    evaluate scripts

--- scripts/superlim_evaluate.py
+++ scripts/superlim_evaluate.py
@@ -12,11 +12,12 @@ vX 20230303
 # Standard Library
 import sys
 import json
-import io # for io.IOBase
-import os # for os.PathLike
+import io  # for io.IOBase
+import os  # for os.PathLike
 
 # SuperLim-specific
 import kralpha
+import evaluate
 
 
 # Auxiliary functions to read datafiles and run a pre-evaluation sanity check
@@ -40,29 +41,29 @@ def _read_data_from_jsonl_file(file_):
             to validate your jsonl file!
 
     """
-    
-    if isinstance(file_,io.IOBase):
-        data = list(map(json.loads,file_))
-        
-    elif isinstance(file_,(str,bytes,os.PathLike)):
+
+    if isinstance(file_, io.IOBase):
+        data = list(map(json.loads, file_))
+
+    elif isinstance(file_, (str, bytes, os.PathLike)):
         with open(file_) as f:
-            data = list(map(json.loads,f))
-            
+            data = list(map(json.loads, f))
+
     else:
         raise TypeError('[Read data] I don\'t know how to read from this kind of '
                         'object: %s. Must be stringlike, pathlike, or filelike.'
                         % (type(file_),))
-        
+
     return data
 
 
-def _sanity_check(gold_data,system_data):
+def _sanity_check(gold_data, system_data):
     """Sanity check to be run before comparing a set of system predictions
     with a gold standard. Checks a number of things, but most
     importantly it inspects the paired items to see if they are the
     same (apart from the label). This helps to make sure that each
     system prediction is compared to the right gold standard item.
-   
+
     Args:
         gold_data: a list of dictionaries containing the gold standard items
         system_data: a list of dictionaries containing the items with
@@ -74,16 +75,16 @@ def _sanity_check(gold_data,system_data):
             is given in terms of (guessed) linenumbers. If your data does 
             not come directly from a file, these may be wrong.
 
-    """    
+    """
     if len(gold_data) != len(system_data):
         raise Exception('[Sanity check] Gold and system data files are of '
                         'differing length.')
-    
-    for i,gold_datapoint in enumerate(gold_data):
 
-        system_datapoint = system_data[i].copy() # NOTE! Copy, to be mutated!
-        
-        if not isinstance(system_datapoint,dict):
+    for i, gold_datapoint in enumerate(gold_data):
+
+        system_datapoint = system_data[i].copy()  # NOTE! Copy, to be mutated!
+
+        if not isinstance(system_datapoint, dict):
             raise Exception('[Sanity check] Line %s in the system data file is '
                             'not a JSON object (but array, string, number, etc).'
                             % (i+1,))
@@ -93,8 +94,14 @@ def _sanity_check(gold_data,system_data):
                             'does not have a \'label\' attribute.'
                             % (i+1,))
 
-        system_datapoint['label'] = gold_datapoint['label']        
+        system_datapoint['label'] = gold_datapoint['label']
+        if "prediction" in system_datapoint:
+            system_datapoint.pop("prediction")
+        # TODO fix for added prediction elemente
         if system_datapoint != gold_datapoint:
+            print(system_datapoint)
+            print(gold_datapoint)
+            print("#"*50)
             raise Exception('[Sanity check] Line %s in the gold and system data '
                             'files differ (not just in label).'
                             % (i+1,))
@@ -103,11 +110,9 @@ def _sanity_check(gold_data,system_data):
     return
 
 
-
-
 # Generic evaluation functions
 #
-def _evaluate_alphanominal(gold_file,system_file):
+def _evaluate_alphanominal(gold_file, system_file):
     """Evaluate nominal scale system predictions in system_file against the
     gold standard in gold_file, and report as Krippendorff's α.
 
@@ -119,13 +124,17 @@ def _evaluate_alphanominal(gold_file,system_file):
         a dictionary with key 'alpha' for Krippendorff's α for nominal
         data (float), and a key 'N' for the number of data points
         (integer).
-    """    
+    """
     gold_data = _read_data_from_jsonl_file(gold_file)
     system_data = _read_data_from_jsonl_file(system_file)
-    _sanity_check(gold_data,system_data) 
+    _sanity_check(gold_data, system_data)
 
     gold_labels = [datapoint['label'] for datapoint in gold_data]
-    system_labels = [datapoint['label'] for datapoint in system_data]
+    
+    if "prediction" in system_data[0]:
+        system_labels = [datapoint['prediction'] for datapoint in system_data]
+    else:
+        system_labels = [datapoint['label'] for datapoint in system_data]
 
     R = {}
     R['N'] = len(gold_labels)
@@ -135,7 +144,7 @@ def _evaluate_alphanominal(gold_file,system_file):
     return R
 
 
-def _evaluate_alphainterval(gold_file,system_file):
+def _evaluate_alphainterval(gold_file, system_file):
     """Evaluate interval scale system predictions in system_file against the
     gold standard in gold_file, and report as Krippendorff's α.
 
@@ -147,13 +156,16 @@ def _evaluate_alphainterval(gold_file,system_file):
         a dictionary with key 'alpha' for Krippendorff's α for
         interval data (float), and a key 'N' for the number of data
         points (integer).
-    """    
+    """
     gold_data = _read_data_from_jsonl_file(gold_file)
     system_data = _read_data_from_jsonl_file(system_file)
-    _sanity_check(gold_data,system_data) 
+    _sanity_check(gold_data, system_data)
 
     gold_labels = [datapoint['label'] for datapoint in gold_data]
-    system_labels = [datapoint['label'] for datapoint in system_data]
+    if "prediction" in system_data[0]:
+        system_labels = [datapoint['prediction'] for datapoint in system_data]
+    else:
+        system_labels = [datapoint['label'] for datapoint in system_data]
 
     R = {}
     R['N'] = len(gold_labels)
@@ -163,7 +175,7 @@ def _evaluate_alphainterval(gold_file,system_file):
     return R
 
 
-def _evaluate_accuracy(gold_file,system_file):
+def _evaluate_accuracy(gold_file, system_file):
     """Evaluate system predictions in system_file against the gold
     standard in gold_file, and report as accuracy (=proportion of
     correct predictions).
@@ -181,21 +193,24 @@ def _evaluate_accuracy(gold_file,system_file):
         a dictionary with key 'accuracy' for accuracy (float) for and
         a key 'N' for the number of data points (integer).
 
-    """    
+    """
     gold_data = _read_data_from_jsonl_file(gold_file)
     system_data = _read_data_from_jsonl_file(system_file)
-    _sanity_check(gold_data,system_data) 
+    _sanity_check(gold_data, system_data)
 
     gold_labels = [datapoint['label'] for datapoint in gold_data]
-    system_labels = [datapoint['label'] for datapoint in system_data]
-    
-    correct = sum(gold_label==system_label
-                  for gold_label,system_label in zip(gold_labels,system_labels))
+    if "prediction" in system_data[0]:
+        system_labels = [datapoint['prediction'] for datapoint in system_data]
+    else:
+        system_labels = [datapoint['label'] for datapoint in system_data]
+
+    correct = sum(gold_label == system_label
+                  for gold_label, system_label in zip(gold_labels, system_labels))
 
     R = {}
     R['N'] = len(gold_labels)
     R['accuracy'] = correct/R['N']
-    
+
     return R
 
 
@@ -208,90 +223,114 @@ def _evaluate_accuracy(gold_file,system_file):
 # in the same way as the _evaluate_YYYY functions above, and return a
 # dictionary with information.
 #
-#   absabank-imm                   Alpha (interval) 
-#   argumentation_sentences        Alpha (nominal) 
-#   dalaj-ged-superlim 	           Alpha (nominal) 
-#   supersim-superlim-relatedness  Alpha (interval) 
-#   supersim-superlim-similarity   Alpha (interval) 
-#   sweanalogy 	                   Accuracy (~= Pseudoalpha) 
-#   swediagnostics 	           Alpha (nominal) 
+#   absabank-imm                   Alpha (interval)
+#   argumentation_sentences        Alpha (nominal)
+#   dalaj-ged-superlim 	           Alpha (nominal)
+#   supersim-superlim-relatedness  Alpha (interval)
+#   supersim-superlim-similarity   Alpha (interval)
+#   sweanalogy 	                   Accuracy (~= Pseudoalpha)
+#   swediagnostics 	           Alpha (nominal)
 #   swedn 	                   Harmonic mean ('F1') of Rouge and Bleu
 #                                      ^^^NOT CURRENTLY IMPLEMENTED!
 #   swefaq 	                   Pseudoalpha
-#                                      = (Accuracy - 109/2049) / (1940/2049) 
-#   swemnli 	                   Alpha (nominal) 
-#   sweparaphrase 	           Alpha (interval) 
-#   swesat-synonyms 	           Pseudoalpha = (Accuracy - 1/5) / (4/5) 
-#   swewic 	                   Alpha (nominal) 
+#                                      = (Accuracy - 109/2049) / (1940/2049)
+#   swenli 	                   Alpha (nominal)
+#   sweparaphrase 	           Alpha (interval)
+#   swesat-synonyms 	           Pseudoalpha = (Accuracy - 1/5) / (4/5)
+#   swewic 	                   Alpha (nominal)
 #   swewinogender 	           Alpha (nominal) and parity: number of items with
-#                                        identical tuple_ids that get the same label 
-#   swewinograd                    Alpha (nominal) 
+#                                        identical tuple_ids that get the same label
+#   swewinograd                    Alpha (nominal)
 
-def evaluate_absabank_imm(gold_file,system_file):
-    return _evaluate_alphainterval(gold_file,system_file)
-
-
-def evaluate_argumentation_sentences(gold_file,system_file):
-    return _evaluate_alphanominal(gold_file,system_file)
+def evaluate_absabank_imm(gold_file, system_file):
+    return _evaluate_alphainterval(gold_file, system_file)
 
 
-def evaluate_dalaj_ged_superlim(gold_file,system_file):
-    return _evaluate_alphanominal(gold_file,system_file)
+def evaluate_argumentation_sentences(gold_file, system_file):
+    return _evaluate_alphanominal(gold_file, system_file)
 
 
-def evaluate_supersim_superlim_relatedness(gold_file,system_file):
-    return _evaluate_alphainterval(gold_file,system_file)
+def evaluate_dalaj_ged_superlim(gold_file, system_file):
+    return _evaluate_alphanominal(gold_file, system_file)
 
 
-def evaluate_supersim_superlim_similarity(gold_file,system_file):
-    return _evaluate_alphainterval(gold_file,system_file)
+def evaluate_supersim_superlim_relatedness(gold_file, system_file):
+    return _evaluate_alphainterval(gold_file, system_file)
 
 
-def evaluate_sweanalogy(gold_file,system_file):
-    R = _evaluate_accuracy(gold_file,system_file)
+def evaluate_supersim_superlim_similarity(gold_file, system_file):
+    return _evaluate_alphainterval(gold_file, system_file)
+
+
+def evaluate_sweanalogy(gold_file, system_file):
+    R = _evaluate_accuracy(gold_file, system_file)
     R['pseudoalpha'] = R['accuracy']
     return R
 
 
-def evaluate_swediagnostics(gold_file,system_file):
-    return _evaluate_alphanominal(gold_file,system_file)
+def evaluate_swediagnostics(gold_file, system_file):
+    return _evaluate_alphanominal(gold_file, system_file)
 
 
-def evaluate_swedn(gold_file,system_file):
+def evaluate_swedn(gold_file, system_file):
+    # measures
+    # rouge = evaluate.load("rouge")
+    # bleu = evaluate.load("bleu")
+    purple = evaluate.combine(["rouge", "bleu"])
+
+    label = "summary"
+    gold_data = _read_data_from_jsonl_file(gold_file)
+    system_data = _read_data_from_jsonl_file(system_file)
+    # _sanity_check(gold_data, system_data)
+
+    gold_labels = [[datapoint[label]] for datapoint in gold_data]
+    
+    if "prediction" in system_data[0]:
+        system_labels = [datapoint['prediction'] for datapoint in system_data]
+    else:
+        system_labels = [datapoint[label] for datapoint in system_data]
+    
+    results = purple.compute(references=gold_labels, predictions=system_labels)
+    print(results)
+    results["f1"] = 2 * results["bleu"] * results["rouge"] / (results["rouge"] + results["bleu"])
+    return results
     raise NotImplementedError()
 
 
-def evaluate_swefaq(gold_file,system_file):
-    R = _evaluate_accuracy(gold_file,system_file)
-    R['pseudoalpha'] = (R['accuracy'] - 109/2049) / (1940/2049) 
+def evaluate_swefaq(gold_file, system_file):
+    R = _evaluate_accuracy(gold_file, system_file)
+    R['pseudoalpha'] = (R['accuracy'] - 109/2049) / (1940/2049)
     return R
 
 
-def evaluate_swemnli(gold_file,system_file):
-    return _evaluate_alphanominal(gold_file,system_file)
+def evaluate_swenli(gold_file, system_file):
+    return _evaluate_alphanominal(gold_file, system_file)
 
 
-def evaluate_paraphrase(gold_file,system_file):
-    return _evaluate_alphainterval(gold_file,system_file)
+def evaluate_paraphrase(gold_file, system_file):
+    return _evaluate_alphainterval(gold_file, system_file)
 
 
-def evaluate_swesat_synonyms(gold_file,system_file):
-    R = _evaluate_accuracy(gold_file,system_file)
-    R['pseudoalpha'] = (R['accuracy'] - 1/5) / (4/5) 
+def evaluate_swesat_synonyms(gold_file, system_file):
+    R = _evaluate_accuracy(gold_file, system_file)
+    R['pseudoalpha'] = (R['accuracy'] - 1/5) / (4/5)
     return R
 
 
-def evaluate_swewic(gold_file,system_file):
-    return _evaluate_alphanominal(gold_file,system_file)
+def evaluate_swewic(gold_file, system_file):
+    return _evaluate_alphanominal(gold_file, system_file)
 
 
-def evaluate_swewinogender(gold_file,system_file):
+def evaluate_swewinogender(gold_file, system_file):
     gold_data = _read_data_from_jsonl_file(gold_file)
     system_data = _read_data_from_jsonl_file(system_file)
-    _sanity_check(gold_data,system_data) 
+    _sanity_check(gold_data, system_data)
 
     gold_labels = [datapoint['label'] for datapoint in gold_data]
-    system_labels = [datapoint['label'] for datapoint in system_data]
+    if "prediction" in system_data[0]:
+        system_labels = [datapoint['prediction'] for datapoint in system_data]
+    else:
+        system_labels = [datapoint['label'] for datapoint in system_data]
     tuple_ids = [datapoint['meta']['tuple_id'] for datapoint in gold_data]
 
     R = {}
@@ -304,24 +343,22 @@ def evaluate_swewinogender(gold_file,system_file):
     # R = _evaluate_alphanominal(gold_file,system_file)
     # Parity looks at how often the prediction is the same accross
     # tuple_id, irrespective of whether this prediction is correct.
-    system_labels_by_tuple_id = {tuple_id:[] for tuple_id in tuple_ids}
-    for tuple_id,system_label in zip(tuple_ids,system_labels):
+    system_labels_by_tuple_id = {tuple_id: [] for tuple_id in tuple_ids}
+    for tuple_id, system_label in zip(tuple_ids, system_labels):
         system_labels_by_tuple_id[tuple_id].append(system_label)
 
     identical = sum(label1 == label2 == label3
-                    for label1,label2,label3 in system_labels_by_tuple_id.values())
+                    for label1, label2, label3 in system_labels_by_tuple_id.values())
     R['parity'] = identical/R['N']
-    
+
     return R
 
 
-def evaluate_swewinograd(gold_file,system_file):
-    return _evaluate_alphanominal(gold_file,system_file)
+def evaluate_swewinograd(gold_file, system_file):
+    return _evaluate_alphanominal(gold_file, system_file)
 
 
-
-
-if __name__=='__main__':
+if __name__ == '__main__':
 
     # Just a silly example, only testing the swewic evaluation code
     # and an unnecessary use of pathlib
@@ -329,6 +366,4 @@ if __name__=='__main__':
     gold_filepath = Path(sys.argv[1])
     system_filepath = Path(sys.argv[2])
 
-    print(evaluate_swewic(gold_filepath,system_filepath))
-
-    
+    print(evaluate_swewic(gold_filepath, system_filepath))
